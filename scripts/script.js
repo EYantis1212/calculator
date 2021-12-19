0f2555f80f9d64f963b7ee147e78543b57e443b7
Yantis <yantis1212@outlook.com>
2021-12-18 21:11:45 -0700

    Added Selectors and Math Functions

---
+++ scripts/script.js
@@ -0,0 +1,63 @@
+// SELECTORS
+
+const numberBtns = document.querySelectorAll('button .number');
+const operatorBtns = document.querySelectorAll('button .operator');
+const clearBtns = document.querySelectorAll('button .clear');
+const equalsBtn = document.querySelectorAll('button .equals');
+const posNegBtn = document.querySelectorAll('button .posNeg');
+const dotBtn = document.querySelectorAll('button .dot');
+const displayCurrent = document.getElementById('display-current');
+const displayRunning = document.getElementById('display-running');
+
+// VARIABLES
+operand1 = '';
+operand2 = '';
+
+//LISTENERS
+numberBtns.forEach((i) =>
+	i.addEventListener('click', () => appendNum(button.value))
+);
+
+// APPEND FUNCTION
+function appendNum(num) {
+	displayCurrent.textContent += num;
+}
+// MATH FUNCTIONS
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+function subtract(num1, num2) {
+	return num1 - num2;
+}
+
+function multiply(num1, num2) {
+	return num1 * num2;
+}
+
+function divide(num1, num2) {
+	return num1 / num2;
+}
+
+function percent(num1) {
+	return num1 / 100;
+}
+
+// NEGATIVE FUNCTION
+
+function posNeg(num1) {
+	if (num1 > 0) {
+		return -Math.abs(num1);
+	} else if (num1 < 0) {
+		return Math.abs(num1);
+	} else return num1;
+}
+
+// BACKSPACE FUNCTION
+
+// CLEAR FUNCTION
+
+// DOT FUNCTION
+
+// OPERATE FUNCTION
